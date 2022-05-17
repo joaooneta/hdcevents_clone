05b7ab76fb999b6e7982e964f8d7aed71aea09a6
João Oneta <joaomatheusoneta01@gmail.com>
2022-05-17 20:50:17 -0300

    Desenvolvido um layout padrão para a página utilizando o Blade, através das diretivas @yield, além do início do desenvolvimento da View welcome, resgatando dados sobre os eventos do banco de dados com o seu model Event, juntamente com o auxílio do seu controller EvenController, para exibi-los através da sua respectiva view.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <!-- HEADER -->
+  <header class="header">
+    <!-- BRAND -->
+    <a href="/"><img class="header-brand" src="/img/hdcevents_logo.svg" alt="HDC Events logo"></a>
+
+    <!-- NAV -->
+    <nav class="main-nav">
+      <!-- NAV LIST -->
+      <ul class="main-nav-list">
+        <li><a class="main-nav-link" href="/">Eventos</a></li>
+        <li><a class="main-nav-link" href="/">Criar Eventos</a></li>
+        <li><a class="main-nav-link" href="/">Entrar</a></li>
+        <li><a class="main-nav-link" href="/">Cadastrar</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- VIEW CONTENT -->
+  @yield('content')
+
+  <!-- FOOTER -->
+  <footer class="footer">HDC Events &copy; 2022</footer>
+</body>
+</html>
