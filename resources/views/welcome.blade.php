ff0dc7f5e3f21f69ef3fac9a8a19446337cebe02
João Oneta <joaomatheusoneta01@gmail.com>
2022-05-27 23:24:08 -0300

    Implementada a busca de eventos por título no path /. A busca é realizada através do Eloquent, no model Event, com os métodos where/get. Além disso, foram incluídos feedbacks visuais de acordo com os resultados das pesquisas do usuário.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <section class="section-search">
         <div class="search-container">
             <h1 class="heading heading-primary">Busque um evento</h1>
-            <form class="search-form" action="">
+            <form class="search-form" action="/" method="GET">
                 <input type="text" name="search" id="search" label="Procure um evento">
             </form>
         </div>
@@ -16,7 +16,11 @@
     <!-- SECTION EVENTS -->
     <section class="section-events">
         <div class="container">
-            <h2 class="heading heading-secondary">Próximos Eventos</h2>
+            @if($search)
+                <h2 class="heading heading-secondary">Buscando por {{ $search }}</h2>
+            @else
+                <h2 class="heading heading-secondary">Próximos Eventos</h2>
+            @endif
             <h3 class="heading heading-terciary">Veja os eventos dos próximos dias</h3>
         </div>
     </section>
@@ -29,7 +33,9 @@
         <a href="/events/{{ $event->id }}">Saber mais</a>
         <br><br>
     @endforeach
-    @if (count($events) == 0)
+    @if (count($events) == 0 && $search)
+        <p>Nenhum evento encontrado com o título {{ $search }}, <a href="/">Visualizar todos os eventos</a></p>
+    @elseif(count($events) == 0)
         <p>Nenhum evento registrado</p>
     @endif
 @endsection
